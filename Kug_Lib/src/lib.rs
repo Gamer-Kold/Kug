d84b3ccab3cba481e1f30a19c98e021376b88e2e
TaromaruYuki <github.7wmif@simplelogin.com>
2023-05-12 20:32:32 -0400

    Kug_Lib Hello World example

--- Kug_Lib/src/lib.rs
+++ Kug_Lib/src/lib.rs
@@ -1,6 +1,25 @@
 use gdnative::prelude::*;
 
+#[derive(NativeClass)]
+#[inherit(Node)]
+pub struct HelloWorld;
+
+impl HelloWorld {
+    fn new(_base: &Node) -> Self {
+        HelloWorld
+    }
+}
+
+#[methods]
+impl HelloWorld {
+    #[method]
+    fn _ready(&self, #[base] base: &Node) {
+        godot_print!("Hello world from node {}!", base.to_string());
+    }
+}
+
 fn init(handle : InitHandle){
+    handle.add_class::<HelloWorld>();
 }
 
 godot_init!(init);
