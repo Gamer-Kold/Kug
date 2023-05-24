0b550f6641f9ff61739a8857cee36d1cedb506ce
TaromaruYuki <github.7wmif@simplelogin.com>
2023-05-24 19:19:27 -0400

    Iq stanza decoding

--- Kug_Lib/xmpp/src/packet.rs
+++ Kug_Lib/xmpp/src/packet.rs
@@ -1,10 +1,12 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, str::FromStr};
 use std::str;
-use xmpp_parsers::Element;
+use xml::{reader::{EventReader, XmlEvent}, name::OwnedName, attribute::OwnedAttribute};
+use xmpp_parsers::{Element, iq::Iq, message::Message, presence::Presence};
 
-type PacketExportResult = Result<String, PacketExportError>;
+type PacketExportResult = Result<String, PacketError>;
+type PacketImportResult = Result<Packet, PacketError>;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Packet {
     Start(HashMap<String, String>),
     Stanza(Element),
@@ -12,7 +14,7 @@ pub enum Packet {
 }
 
 #[derive(Debug)]
-pub enum PacketExportError {
+pub enum PacketError {
     InvalidPacketType,
 }
 
@@ -50,6 +52,44 @@ impl PacketExporter {
             return Ok(result);
         }
 
-        Err(PacketExportError::InvalidPacketType)
+        Err(PacketError::InvalidPacketType)
+    }
+}
+
+pub struct PacketImporter;
+
+impl PacketImporter {
+    fn convert_attributes(attributes: Vec<OwnedAttribute>) -> HashMap<String, String> {
+        let mut converted_attributes: HashMap<String, String> = HashMap::new();
+
+        for attribute in attributes {
+            converted_attributes.insert(attribute.name.to_string(), attribute.value);
+        }
+
+        converted_attributes
+    }
+    
+    pub fn import(content: String) -> PacketImportResult {
+        let mut parser = EventReader::new(content.as_bytes());
+        parser.next().unwrap(); // Skip XmlEvent::StartDocument
+        let parsed_root = parser.next().unwrap();
+
+        if let XmlEvent::StartElement { name, attributes, namespace } = parsed_root {
+            let converted_attributes = PacketImporter::convert_attributes(attributes);
+
+            if name.prefix.is_some() {
+                // This element is a stream:stream (No other elements we will parse have a prefix)
+                return Ok(Packet::Start(converted_attributes));
+            }
+
+            match name.local_name.as_str() {
+                "presence" => todo!("presence"),
+                "message" => todo!("message"),
+                "iq" => Ok(Packet::Stanza(Element::from_reader_with_prefixes(content.as_bytes(), String::from("jabber:client")).unwrap())),
+                _ => todo!(),
+            }
+        } else {
+            todo!("XML was {:?}", parsed_root);
+        }
     }
 }
